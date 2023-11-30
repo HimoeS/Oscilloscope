c970831c5654fb1df96725bf0a09c6b235603f18
Admin <tomyng2519@gmail.com>
2023-11-30 20:02:50 +0700

    first commit

---
+++ AndroidCode/ESP32BLUETOOTH/app/src/main/java/com/example/esp32bluetooth/MainActivity.kt
@@ -0,0 +1,348 @@
+package com.example.esp32bluetooth
+
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothGatt
+import android.bluetooth.BluetoothGattCallback
+import android.bluetooth.BluetoothGattCharacteristic
+import android.bluetooth.BluetoothGattDescriptor
+import android.bluetooth.BluetoothManager
+import android.bluetooth.BluetoothProfile
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
+import android.bluetooth.le.ScanSettings
+import android.content.pm.ActivityInfo
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.os.Looper
+import android.os.ParcelUuid
+import android.os.SystemClock
+import android.util.Log
+import android.view.View
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.jjoe64.graphview.GraphView
+import com.jjoe64.graphview.helper.StaticLabelsFormatter
+import com.jjoe64.graphview.series.DataPoint
+import com.jjoe64.graphview.series.LineGraphSeries
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.util.UUID
+import kotlin.math.abs
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val graph: GraphView by lazy {
+        findViewById(R.id.graphView)
+    }
+
+    private val dataTextView: TextView by lazy {
+        findViewById(R.id.dataTextView)
+    }
+
+    private val maxDataTextView: TextView by lazy {
+        findViewById(R.id.maxDataTextView)
+    }
+
+    private val startButton: TextView by lazy {
+        findViewById(R.id.startButton)
+    }
+
+    private val stopButton: TextView by lazy {
+        findViewById(R.id.stopButton)
+    }
+
+    private val scaleButton: TextView by lazy {
+        findViewById(R.id.scaleButton)
+    }
+
+    private val resetButton: TextView by lazy {
+        findViewById(R.id.resetButton)
+    }
+
+    private var isUpdatingGraph = false
+    private var currentData = 0.0
+    private var Vmax = 0.0
+
+    private val serviceUuid = UUID.fromString("000000ff-0000-1000-8000-00805f9b34fb")
+    private val characteristicUuid = UUID.fromString("beb5483e-36e1-4688-b7f5-ea07361b26a8")
+
+    private val bluetoothManager by lazy { getSystemService(BLUETOOTH_SERVICE) as BluetoothManager }
+    private val bluetoothAdapter: BluetoothAdapter by lazy { bluetoothManager.adapter }
+    private val bluetoothLeScanner by lazy { bluetoothAdapter.bluetoothLeScanner }
+
+    private val bluetoothDataHandlerThread = HandlerThread("BluetoothDataHandlerThread")
+    private lateinit var bluetoothDataHandler: Handler
+
+    private var gatt: BluetoothGatt? = null
+
+    private val scanCallback = object : ScanCallback() {
+        override fun onScanResult(callbackType: Int, result: ScanResult?) {
+            result?.device?.let { device ->
+                checkPermission()
+                Log.d("Bluetooth Scan", "Found device: ${device.name} with address: ${device.address}")
+                if (device.name == "ESP32") {
+                    Log.d("Bluetooth Scan", "Connecting to ESP32")
+                    connectToDevice(device)
+                }
+            }
+        }
+    }
+
+    private val gattCallback = object : BluetoothGattCallback() {
+        override fun onConnectionStateChange(gatt: BluetoothGatt, status: Int, newState: Int) {
+            checkPermission()
+            if (newState == BluetoothProfile.STATE_CONNECTED) {
+                Log.d("Bluetooth GATT", "Connected")
+                gatt.discoverServices()
+            } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                Log.d("Bluetooth", "Disconnected")
+            }
+        }
+
+        override fun onServicesDiscovered(gatt: BluetoothGatt, status: Int) {
+            val service = gatt.getService(serviceUuid)
+            val characteristic = service.getCharacteristic(characteristicUuid)
+            checkPermission()
+
+            gatt.setCharacteristicNotification(characteristic, true)
+            val descriptor = characteristic.getDescriptor(UUID.fromString("000000ff-0000-1000-8000-00805f9b34fb"))
+            descriptor.value = BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE
+            gatt.writeDescriptor(descriptor)
+            Log.d("Bluetooth GATT", "Characteristic enabled")
+        }
+
+        override fun onCharacteristicChanged(gatt: BluetoothGatt, characteristic: BluetoothGattCharacteristic) {
+            bluetoothDataHandler.post {
+                val data = characteristic.value
+                val receivedDouble = bytesToDouble(data)
+                val formattedDouble = String.format("%.3f", receivedDouble)
+
+                runOnUiThread {
+                    currentData = receivedDouble
+                    if(receivedDouble>Vmax){
+                        Vmax = receivedDouble
+                        maxDataTextView.text = String.format("%.3f", Vmax)
+                    }
+                    dataTextView.text = formattedDouble
+                }
+            }
+        }
+    }
+
+    private fun bytesToDouble(bytes: ByteArray): Double {
+        try {
+            val buffer = ByteBuffer.wrap(bytes).order(ByteOrder.LITTLE_ENDIAN)
+            return buffer.double
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return 0.0
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bluetoothDataHandlerThread.start()
+        bluetoothDataHandler = Handler(bluetoothDataHandlerThread.looper)
+
+        checkPermission()
+        startScan()
+
+        val staticLabelsFormatter = StaticLabelsFormatter(graph)
+        staticLabelsFormatter.setHorizontalLabels(arrayOf("", "", "", "", ""))
+        staticLabelsFormatter.setVerticalLabels(arrayOf("", "", "", "", ""))
+        graph.gridLabelRenderer.labelFormatter = staticLabelsFormatter
+
+        setupButton()
+    }
+
+    val series = LineGraphSeries<DataPoint>()
+    var reset = false
+
+    private fun updateGraphWithBluetoothData() {
+        if (isUpdatingGraph) {
+            val mHandler = Handler(Looper.getMainLooper())
+            val mTimer: Runnable
+
+            var x = series.highestValueX + 0.1
+
+
+
+            series.color = Color.WHITE
+            graph.addSeries(series)
+            //var count = 0
+
+            mTimer = object : Runnable {
+                override fun run() {
+                    if (isUpdatingGraph) {
+                        if (x >= 50.0) {
+                            x = 0.0
+                            series.resetData(arrayOf<DataPoint>())
+                        }
+                        if (reset){
+                            Log.d("reset", "reset to 0")
+                            x=0.0
+                            series.resetData(arrayOf<DataPoint>())
+                            reset = false
+                        }
+
+                        val y = currentData
+
+                        val dataPoint = DataPoint(x, y)
+
+                        series.appendData(dataPoint, false, 500)
+
+                        if (scale) {
+                            Log.d("scale", "Scale = true")
+                            graph.viewport.setMinX(0.0)
+                            graph.viewport.setMaxX(50.0)
+                            graph.viewport.setMinY(5.0)
+                            graph.viewport.setMaxY(0.0)
+                        }
+
+                        x += 0.1
+
+                    }
+                    mHandler.postDelayed(this, 25)
+                }
+            }
+            mHandler.postDelayed(mTimer, 25)
+        } else {
+            Toast.makeText(this, "already running", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+    private fun setupButton() {
+        startButton.setSafeOnClickListener{
+            Toast.makeText(this,"start update",Toast.LENGTH_SHORT).show()
+            isUpdatingGraph = true
+            updateGraphWithBluetoothData()
+        }
+
+        stopButton.setSafeOnClickListener{
+            Toast.makeText(this,"stop update",Toast.LENGTH_SHORT).show()
+            isUpdatingGraph = false
+            Log.d("stopbtn", "Stop button clicked")
+        }
+
+        scaleButton.setSafeOnClickListener {
+            scale = scale != true
+        }
+
+        resetButton.setSafeOnClickListener{
+            Toast.makeText(this,"reset data",Toast.LENGTH_SHORT).show()
+            isUpdatingGraph = true
+            reset =true
+            series.resetData(arrayOf<DataPoint>())
+        }
+
+    }
+
+    var scale = false
+
+    override fun onResume() {
+        super.onResume()
+        checkPermission()
+        //startScan()
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        isUpdatingGraph = true
+    }
+
+    override fun onPause() {
+        super.onPause()
+        isUpdatingGraph = false
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        checkPermission()
+        bluetoothLeScanner.stopScan(scanCallback)
+        gatt?.disconnect()
+        gatt?.close()
+        isUpdatingGraph = false
+        bluetoothDataHandlerThread.quitSafely()
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+    }
+
+
+    private fun startScan() {
+        checkPermission()
+        val scanFilters = mutableListOf<android.bluetooth.le.ScanFilter>()
+        val scanSettings = ScanSettings.Builder()
+            .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+            .build()
+
+        val uuid = UUID.fromString("000000ff-0000-1000-8000-00805f9b34fb") // UUID dịch vụ BLE cần quét
+        val parcelUuid = ParcelUuid(uuid)
+        val scanFilter = android.bluetooth.le.ScanFilter.Builder()
+            .setServiceUuid(parcelUuid)
+            .build()
+        scanFilters.add(scanFilter)
+
+        bluetoothLeScanner.startScan(scanFilters, scanSettings, scanCallback)
+        Log.d("Bluetooth Scan", "Scanning started")
+    }
+
+    private fun connectToDevice(device: BluetoothDevice) {
+        checkPermission()
+        gatt = device.connectGatt(this, true, gattCallback)
+        Log.d("Bluetooth GATT", "Connecting to GATT server")
+    }
+
+    fun View.setSafeOnClickListener(onSafeClick: (View) -> Unit) {
+
+        val safeClickListener = SafeClickListener {
+            onSafeClick(it)
+        }
+        setOnClickListener(safeClickListener)
+    }
+
+    class SafeClickListener(
+        private var defaultInterval: Int = 1000,
+        private val onSafeCLick: (View) -> Unit
+    ) : View.OnClickListener {
+
+        private var lastTimeClicked: Long = 0
+
+        override fun onClick(v: View) {
+            if (SystemClock.elapsedRealtime() - lastTimeClicked < defaultInterval) {
+                return
+            }
+            lastTimeClicked = SystemClock.elapsedRealtime()
+            onSafeCLick(v)
+        }
+    }
+
+    private fun checkPermission(): Boolean {
+        val permissions = arrayOf(
+            Manifest.permission.BLUETOOTH_ADMIN,
+            Manifest.permission.BLUETOOTH_CONNECT,
+            Manifest.permission.BLUETOOTH_SCAN,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+
+        return if (ContextCompat.checkSelfPermission(this.applicationContext, permissions[0]) == PackageManager.PERMISSION_GRANTED &&
+            ContextCompat.checkSelfPermission(this.applicationContext, permissions[1]) == PackageManager.PERMISSION_GRANTED &&
+            ContextCompat.checkSelfPermission(this.applicationContext, permissions[2]) == PackageManager.PERMISSION_GRANTED &&
+            ContextCompat.checkSelfPermission(this.applicationContext, permissions[3]) == PackageManager.PERMISSION_GRANTED
+        ) {
+            Log.d("permission", "Permission checked")
+            true
+        } else {
+            ActivityCompat.requestPermissions(this, permissions, 1)
+            false
+        }
+    }
+}
---
+++ ArduinoCode/ESP32_BLE.ino
@@ -0,0 +1,164 @@
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include <esp_adc_cal.h>
+
+// Địa chỉ UUID dịch vụ
+#define SERVICE_UUID "000000ff-0000-1000-8000-00805f9b34fb"
+// Địa chỉ UUID đặc trưng
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+// Thông số sóng sine
+float amplitude = 0.75;  // Amplitude (độ lớn) của sóng sine (đưa về nửa giá trị ADC)
+float frequency = 1.0;   // Tần số của sóng sine (Hz)
+
+// Thông số xung vuông
+float squareAmplitude = 1.65;  // Amplitude (độ lớn) của xung vuông (đưa về nửa giá trị ADC)
+float squareFrequency = 1.0;   // Tần số của xung vuông (Hz)
+
+// Chân kết nối
+int sinePin = 25;
+int squarePin = 26;
+
+BLEServer* pServer;
+BLEService* pService;
+BLECharacteristic* pCharacteristic;
+
+bool deviceConnected = false; // Biến để theo dõi thiết bị có kết nối hay không
+const int sensorPin = 33;
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+      Serial.println("Thiết bị đã kết nối");
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+      Serial.println("Thiết bị đã ngắt kết nối");
+      delay(1000);
+    }
+};
+
+void setup() {
+  Serial.begin(115200);
+
+  adc1_config_width(ADC_WIDTH_BIT_12);
+  adc1_config_channel_atten(ADC1_CHANNEL_0, ADC_ATTEN_DB_11);
+
+  adc_chars = (esp_adc_cal_characteristics_t *)calloc(1, sizeof(esp_adc_cal_characteristics_t));
+  esp_adc_cal_value_t val_type = esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, 1100, adc_chars);
+
+  pinMode(sinePin, OUTPUT);
+  pinMode(squarePin, OUTPUT);
+  pinMode(sensorPin, INPUT);
+  
+  Serial.println("BLE preparing ...");
+  BLEDevice::init("ESP32"); // Khởi tạo Bluetooth với tên thiết bị là "ESP32"
+  BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+  
+  pService = pServer->createService(SERVICE_UUID);
+  pCharacteristic = pService->createCharacteristic(
+    CHARACTERISTIC_UUID,
+    BLECharacteristic::PROPERTY_READ |
+    BLECharacteristic::PROPERTY_WRITE
+  );
+
+  // Thiết lập giá trị mặc định cho Characteristic
+  pCharacteristic->setValue("Hello, this is ESP32!");
+
+  pService->start();
+  // Bắt đầu dịch vụ của Bluetooth
+  BLEAdvertising* pAdvertising = pServer->getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->start();
+  Serial.println("Bluetooth đang phát...");
+}
+
+void loop() {
+  unsigned long currentTime = millis();
+
+  // Giá trị sóng sine
+  float sineValue = amplitude * sin(2 * PI * frequency * currentTime / 1000.0) + amplitude;
+
+  // Giá trị xung vuông
+  float squareValue = squareWave(currentTime, squareFrequency) + squareAmplitude;
+
+  // Gửi giá trị đến chân 25 và 26
+  analogWrite(sinePin, map(sineValue, 0, 2 * amplitude, 0, 255));
+  analogWrite(squarePin, map(squareValue, 0, 2 * squareAmplitude, 0, 255));
+
+  // In giá trị của sóng sine và xung vuông
+  Serial.print(sineValue);  // In giá trị sóng sine
+  Serial.print('\t');  // Tab để phân tách giữa sóng sine và xung vuông
+  Serial.print(squareValue);  // In giá trị xung vuông
+
+  double sensorValue = analogReadAdjusted(sensorPin);
+  if (deviceConnected) {
+    // Gửi dữ liệu khi có kết nối Bluetooth
+    uint8_t byteArray[sizeof(double)];
+    memcpy(byteArray, &sensorValue, sizeof(double));
+    sensorValue = (sensorValue / 4096) * 3.3;
+    pCharacteristic->setValue((uint8_t*)&sensorValue, sizeof(sensorValue));
+    Serial.print('\t');
+    Serial.println(sensorValue);
+    pCharacteristic->notify(); // Gửi thông báo cho thiết bị đã kết nối
+  }
+  Serial.println();  // Xuống dòng để tạo dòng mới cho Plotter
+  delay(100);
+}
+
+float squareWave(unsigned long t, float frequency) {
+  float period = 1000.0 / frequency;  // Chu kỳ của xung vuông (ms)
+  return (t % (unsigned long)period < period / 2) ? squareAmplitude : -squareAmplitude;
+}
+
+double analogReadAdjusted(byte pinNumber){
+
+  // Specify the adjustment factors.
+  const double f1 = 1.7111361460487501e+001;
+  const double f2 = 4.2319467860421662e+000;
+  const double f3 = -1.9077375643188468e-002;
+  const double f4 = 5.4338055402459246e-005;
+  const double f5 = -8.7712931081088873e-008;
+  const double f6 = 8.7526709101221588e-011;
+  const double f7 = -5.6536248553232152e-014;
+  const double f8 = 2.4073049082147032e-017;
+  const double f9 = -6.7106284580950781e-021;
+  const double f10 = 1.1781963823253708e-024;
+  const double f11 = -1.1818752813719799e-028;
+  const double f12 = 5.1642864552256602e-033;
+
+  // Specify the number of loops for one measurement.
+  const int loops = 40;
+
+  // Specify the delay between the loops.
+  const int loopDelay = 1;
+
+  // Initialize the used variables.
+  int counter = 1;
+  int inputValue = 0;
+  double totalInputValue = 0;
+  double averageInputValue = 0;
+
+  // Loop to get the average of different analog values.
+  for (counter = 1; counter <= loops; counter++) {
+
+    // Read the analog value.
+    inputValue = analogRead(pinNumber);
+
+    // Add the analog value to the total.
+    totalInputValue += inputValue;
+
+    // Wait some time after each loop.
+    delay(loopDelay);
+  }
+
+  // Calculate the average input value.
+  averageInputValue = totalInputValue / loops;
+
+  // Calculate and return the adjusted input value.
+  return f1 + f2 * pow(averageInputValue, 1) + f3 * pow(averageInputValue, 2) + f4 * pow(averageInputValue, 3) + f5 * pow(averageInputValue, 4) + f6 * pow(averageInputValue, 5) + f7 * pow(averageInputValue, 6) + f8 * pow(averageInputValue, 7) + f9 * pow(averageInputValue, 8) + f10 * pow(averageInputValue, 9) + f11 * pow(averageInputValue, 10) + f12 * pow(averageInputValue, 11);
+}
